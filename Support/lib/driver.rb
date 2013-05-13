aa7d5b89f27f6a510af1ad4197ca9f26a877adb8
Michael Johnston <lastobelus@mac.com>
2013-05-12 17:29:06 -0700

    WIP

--- Support/lib/driver.rb
+++ Support/lib/driver.rb
@@ -4,6 +4,12 @@ require 'rubygems'
 require 'shopifydev'
 require "fileutils"
 
+credentials = {
+  'api_key' => "548a72656b1b6c5aab3c7dbbcce19b5d",
+  'password' => "510eefd9964299c5dc95928ca0291b45",
+  'secret' => "36aacb85b640bd8339901a1c4f45b327",
+  'url' => "kerluke-llc6240.myshopify.com"
+}
 
 d = Shopifydev::Shop.new(credentials)
 path = '/tmp/shopifydev_test'
